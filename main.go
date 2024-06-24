b0787753984e16af15644229d7034afd1a1f13f7
lud <ludwinggsanchez@gmail.com>
2024-06-24 17:44:35 -0500

    devops Despliegue prod
    - Se inicializa xray

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
 	"github.com/udistrital/utils_oas/auditoria"
 	"github.com/udistrital/utils_oas/customerror"
+	"github.com/udistrital/utils_oas/xray"
 )
 
 func main() {
@@ -48,6 +49,7 @@ func main() {
 	}
 
 	apistatus.Init()
+	xray.InitXRay()
 	auditoria.InitMiddleware()
 	beego.ErrorController(&customerror.CustomErrorController{})
 	beego.Run()
